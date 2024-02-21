95faf0eb10b683af14fd1f7167ed2bfebc3a58a0
Nitin Mishra <nitin@cheerful.com>
2024-02-21 12:28:21 -0800

    added password field, created api for create user

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ class customUser(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField()
     phone = models.IntegerField()
+    password = models.CharField(max_length=50)
 
     def __str__(self):
         return self.email
--- app/views.py
+++ app/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
+from django.contrib.auth.hashers import make_password
 from django.views.decorators.csrf import csrf_exempt
 import json
+from .models import customUser
 
 
 # Create your views here.
 
+# Creating user from data recieved as json
 @csrf_exempt
 def createUser(request):
     if request.method == 'POST':
-        print('POST')
+        # We are hitting api by postman so we got data in request.body as json
         data = json.loads(request.body)
-        print(data)
-        return HttpResponse(data)
+        name = data.get('name')
+        email = data.get('email')
+        phone = data.get('phone')
+        password = data.get('password')
+
+        if (name and email and phone and password):
+            try:
+                user = customUser(name=name, email=email, phone=phone, password=password)
+                user.save()
+                return JsonResponse({'message': f"User created with Id: {user.id}"})
+            except:
+                return JsonResponse('Failed to register!!')
+
+        return HttpResponse('Failed to register!!')
 
